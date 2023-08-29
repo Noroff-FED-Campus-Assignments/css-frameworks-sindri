758f3c847f1922f5f7479babf8295eca9dc2bbfa
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2023-08-29 22:53:39 +0200

    styled navbar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -3,10 +3,9 @@ import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
+  { name: 'Home', href: '/', current: true },
+  { name: 'Profile', href: '/profile', current: false },
+  { name: 'Login', href: '/login', current: false },
 ]
 
 function classNames(...classes) {
