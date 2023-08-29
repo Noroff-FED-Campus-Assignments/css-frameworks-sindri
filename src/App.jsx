2a6e07d1b69898a85c871c3c3afae1c23654f5f9
Hallvard-Benan <hallvard.benan@gmail.com>
2023-08-29 12:39:11 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <h1 className="text-xs text-red-600">Hello tailwind</h1>
       <div className="flex-column ">
         <div>1</div>
-        <div>2</div>
+        <div>21234</div>
       </div>
       <header>
         <nav>
