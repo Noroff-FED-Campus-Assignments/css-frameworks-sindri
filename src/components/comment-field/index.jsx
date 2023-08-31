6327f062f7c34ae4fd02c1c78e07ebf6d3144a4a
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2023-08-31 14:30:09 +0200

    changed width on commentfield

--- src/components/comment-field/index.jsx
+++ src/components/comment-field/index.jsx
@@ -5,7 +5,7 @@ export function MakeComment(){
 
 
 <form>
-   <div className="w-2/4 mx-auto md:w-3/4 lg:w-2/4 mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+   <div className="w-full mx-auto md:w-3/4 lg:w-2/4 mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
        <div className="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800   ">
            <label htmlFor="comment" className="sr-only">Your comment</label>
            <textarea id="comment" rows="4" className="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Write a comment..." required></textarea>
