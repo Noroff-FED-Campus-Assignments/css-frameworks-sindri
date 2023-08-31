e9bd9657139e7a2fc3fff97bffe724ea797a6fee
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2023-08-31 11:24:00 +0200

    added styling to commentfeild

--- src/components/comment-field/index.jsx
+++ src/components/comment-field/index.jsx
@@ -3,13 +3,16 @@ export function MakeComment(){
     return(
         <>
 
+
 <form>
    <div className="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
-       <div className="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800 mx-11 ">
+       <div className="w-2/4 mx-auto md:w-3/4 lg:w-2/4">
+       <div className="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800   ">
            <label htmlFor="comment" className="sr-only">Your comment</label>
            <textarea id="comment" rows="4" className="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Write a comment..." required></textarea>
        </div>
-       <div className="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600">
+       </div>
+       <div className="flex items-center justify-between px-3 py-2 border-t dark:border-gray-600 w-2/4 mx-auto md:w-3/4 lg:w-2/4">
            <button type="submit" className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
                Post comment
            </button>
