2f500ae101c75a8bb72ab6f5e4ecb3aa7ef6b119
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2023-09-01 11:39:40 +0200

    added container in homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import ExampleUsersPosts from "../components/example-users-posts";
 import Searchbar from "../components/searchbar";
+
 function HomePage() {
   return (
     <>
-    
+    <div className=" w-2/4 mx-auto">
       <Searchbar/>
+     
       <ExampleUsersPosts />
-
+      </div>
     </>
   );
 }
