48fdd291260617309f91d60265e1879d8b7dd75e
Hallvard-Benan <hallvard.benan@gmail.com>
2023-09-01 11:48:53 +0200

    changed footer styles, added logo

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <>
-      <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+      <footer className="bg-[var(--primary)] rounded-lg shadow dark:bg-gray-900 m-4">
         <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
           <div className="sm:flex sm:items-center sm:justify-between">
             <a
@@ -9,41 +9,50 @@ function Footer() {
               className="flex items-center mb-4 sm:mb-0"
             >
               <img
-                src="https://flowbite.com/docs/images/logo.svg"
+                src="/public/assets/media/sindri-logo.svg"
                 className="h-8 mr-3"
-                alt="Flowbite Logo"
+                alt="Sindri Logo"
               />
-              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
                 SindriConnect
               </span>
             </a>
-            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-blue-1000 sm:mb-0 dark:text-gray-400">
               <li>
-                <a href="#" className="mr-4 hover:underline md:mr-6 ">
+                <a
+                  href="#"
+                  className="mr-4 hover:underline md:mr-6 text-blue-100"
+                >
                   About
                 </a>
               </li>
               <li>
-                <a href="#" className="mr-4 hover:underline md:mr-6">
+                <a
+                  href="#"
+                  className="mr-4 hover:underline md:mr-6 text-blue-100"
+                >
                   Privacy Policy
                 </a>
               </li>
               <li>
-                <a href="#" className="mr-4 hover:underline md:mr-6 ">
+                <a
+                  href="#"
+                  className="mr-4 hover:underline md:mr-6 text-blue-100"
+                >
                   Licensing
                 </a>
               </li>
               <li>
-                <a href="#" className="hover:underline">
+                <a href="#" className="hover:underline text-blue-100">
                   Contact
                 </a>
               </li>
             </ul>
           </div>
           <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-          <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          <span className="block text-sm text-gray-50">
             © 2023{" "}
-            <a href="https://flowbite.com/" className="hover:underline">
+            <a href="#" className="hover:underline text-blue-100">
               SindriConnect™
             </a>
             . All Rights Reserved.
